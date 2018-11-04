c253ff8226ccc6066ce9a2db5dcb74f286442fd7
dmrediger <dmrediger@gmail.com>
2018-11-04 14:05:37 -0600

    Delete unused style and Tab Bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -41,14 +41,6 @@ export default class HomeScreen extends React.Component {
           </View>
 
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
       </View>
     );
   }
@@ -60,13 +52,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
   },
   contentContainer: {
     paddingTop: 30,
@@ -91,7 +76,6 @@ const styles = StyleSheet.create({
   welcomeText: {
     fontSize: 30,
     fontFamily: 'cinzel',
-
   },
   dailyQuoteLabel: {
     alignItems: 'flex-start',
@@ -102,17 +86,6 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
     marginVertical: 10,
     backgroundColor: '#EFEFEF',
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
   },
   sectionLabel: {
     fontSize: 12,
@@ -129,44 +102,5 @@ const styles = StyleSheet.create({
     textAlign: 'left',
     paddingHorizontal: 25,
     paddingVertical: 10,
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
