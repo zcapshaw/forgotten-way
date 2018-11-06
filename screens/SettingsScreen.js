697a542895ab2cdb980ce8480d4becc48f150197
dmrediger <dmrediger@gmail.com>
2018-11-05 20:58:52 -0600

    Cleanup of Commentaries and Header update

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser, Font } from 'expo';
 
 /* Changed title from 'app.json' to 'Meditations'
   * We should go back and update all the file names and classes too */
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'Meditations',
+    title: 'COMMENTARIES',
+    headerStyle: {
+      ...Platform.select({
+        ios: {
+          shadowColor: 'black',
+          shadowOffset: { height: 3 },
+          shadowOpacity: 0.1,
+          shadowRadius: 3,
+        },
+        android: {
+          elevation: 20,
+        },
+      }),
+    },
+    headerTitleStyle: {
+      fontSize: 30,
+      fontFamily: 'cinzel',
+    },
   };
+ 
 
-  render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
-  }
+ render() {
+    return (
+    	<View style={styles.container}>
+        	<ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        	</ScrollView>
+        </View>
+    	);
+	}	
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 10,
+  },
+  });
