dd396c9334ec612c9a3dc5b0db71b75f407a336c
Zach Capshaw <zcapshaw@gmail.com>
2018-11-08 21:00:18 -0700

    more styling tweaks for commentaries

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -96,7 +96,14 @@ export default class HomeScreen extends React.Component {
                 ]}
               horizontal
               showsHorizontalScrollIndicator={false}
-              renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+              renderItem={({ item }) => (
+                <ImageBackground
+                source={{ uri: 'https://images.unsplash.com/photo-1534608176107-b67f671733b3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2eaaaa01848d4a9c31b21a3b9165af6f&auto=format&fit=crop&w=1052&q=80' }}
+                style={styles.item}
+                >
+                  <Text style={styles.itemText}>{item.key}</Text>
+                </ImageBackground>
+              )}
             />
           </View>
 
@@ -183,10 +190,16 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#fff',
     overflow: 'hidden',
-    backgroundColor: '#33adff',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+  itemText: {
     color: '#fff',
     fontSize: 20,
     fontWeight: 'bold',
     fontFamily: 'lato-black',
-  },
+    textAlign: 'center',
+  }
 });
