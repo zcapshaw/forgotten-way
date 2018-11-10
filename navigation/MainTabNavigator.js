b605433b27fccb9ea3e96008c266c6c707624ac2
dmrediger <dmrediger@gmail.com>
2018-11-10 16:45:25 -0600

    Setup new Screen and add react-native-track-player

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -66,7 +66,6 @@ SettingsStack.navigationOptions = {
   ),
 };
 
-
 export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
---
+++ screens/AudioPlayer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import TrackPlayer from 'react-native-track-player';
+
+TrackPlayer.setupPlayer().then(() => {
+	// The player is ready to be used
+});
