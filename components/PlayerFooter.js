679bd7ff3c93e6e058865da725df4e38daaee1c4
dmrediger <dmrediger@gmail.com>
2018-11-18 14:08:19 -0600

    Add isPlaying state change for play/pause icon in PlayerFooter

--- components/PlayerFooter.js
+++ components/PlayerFooter.js
@@ -7,36 +7,50 @@ import { MonoText } from '../components/StyledText';
 
 class PlayerFooter extends React.Component {
 	
-	_onPressButton() {
-      this.props.navigation.navigate('Modal')
-    };
+	_onPressExpandButton() {
+	  this.props.navigation.navigate('Modal')
+	}
+
+	_onPressPlayButton() {
+		this.setState(previousState => (
+			{ isPlaying: !previousState.isPlaying }
+		))
+		//console.log(this.state)
+	}
+
+	constructor(props) {
+		super(props);
+		this.state = { isPlaying: false };
+	}
 
 	render() {
 		return(
 		<View style={styles.playerFooter}>
-          <TouchableWithoutFeedback onPress={this._onPressButton.bind(this)}>
-            <View style={styles.footerIcon}><Ionicons name="ios-arrow-up" size={32} color="white" /></View>
-          </TouchableWithoutFeedback>
-          <Text style={{color:'white', fontSize:16}}>0:34/4:24</Text>
-          <View style={styles.footerIcon}><Ionicons name="ios-play" size={40} color="white" /></View>
-        </View>
+			<TouchableWithoutFeedback onPress={this._onPressExpandButton.bind(this)}>
+				<View style={styles.footerIcon}><Ionicons name="ios-arrow-up" size={32} color="white" /></View>
+			</TouchableWithoutFeedback>
+		  	<Text style={{color:'white', fontSize:16}}>0:34/4:24</Text>
+		  	<TouchableWithoutFeedback onPress={this._onPressPlayButton.bind(this)}>
+				<View style={styles.footerIcon}> {this.state.isPlaying ? <Ionicons name="ios-pause" size={40} color="white" /> : <Ionicons name="ios-play" size={40} color="white" />} </View>
+			</TouchableWithoutFeedback>
+		</View>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
 	playerFooter: {
-	    display: 'flex',
-	    flex: 1,
-	    flexDirection: 'row',
-	    alignItems: 'center',
-	    justifyContent: 'space-between',
-	    backgroundColor: '#545454',
-  	},
+		display: 'flex',
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		backgroundColor: '#545454',
+	},
 
 	footerIcon: {
-    	paddingHorizontal: 25,
-  	},
+		paddingHorizontal: 25,
+	},
 })
 
 export default withNavigation(PlayerFooter);
